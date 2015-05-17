75925f5fbda41ad02638dc0d5d714b4d5e2449c2
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-05-16 17:52:37 -0700

    Git ignore compiled resources

--- resources/public/out/eth/js.cljs
+++
@@ -1,6 +0,0 @@
-(ns eth.js
-  (:require
-    [eth.js.web3]
-    [eth.js.eth]))
-
-(comment "Just load the namespaces!")
--- resources/public/out/eth/js.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 0.0-2740 {}
-goog.provide('eth.js');
-goog.require('cljs.core');
-goog.require('eth.js.eth');
-goog.require('eth.js.web3');
-
-//# sourceMappingURL=js.js.map
