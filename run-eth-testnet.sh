3533610cbdef6f7e562f2f3d0d31f4c7c085ce58
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-06-25 18:31:05 -0700

    testnet/local geth run scripts

---
+++ run-eth-testnet.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#geth --rpc --rpccorsdomain "*" --unlock primary --verbosity 3 --identity "ethereum.cljs" --mine --networkid "4353453510" --datadir "geth-data" 
+geth --rpc --rpccorsdomain "*" --unlock primary --verbosity 3 --identity "ethereum.cljs" console
